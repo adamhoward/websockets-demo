35d10681a9f926143ea225c910640ff3a3dbc77a
Adam Howard <howard.adam@gmail.com>
2012-10-28 18:17:48 -0500

    Serving WebSockets and EventSource from same server.

--- src/websockets_demo/core.clj
+++ src/websockets_demo/core.clj
@@ -5,11 +5,13 @@
 (def ws-connections (atom #{}))
 (def es-connections (atom #{}))
 
+;;; Send a message to each connection
 (defn send-to-subscribers [msg connections]
   (doseq [c connections]
     (println "sending" msg "to" c)
     (.send c msg)))
 
+;;; Every second, send a random number between 0 and 1 to all connections
 (defn send-loop []
   (while true
     (do
@@ -17,9 +19,11 @@
       (send-to-subscribers (EventSourceMessage. (str (rand))) @es-connections)
       (. Thread (sleep 1000)))))
 
+;;; Start the server and the send loop
 (defn -main []
   (println "Starting server...")
   (doto (WebServers/createWebServer 9000)
+    ;; Mount WebSocketHandler at /ws
     (.add "/ws"
           (proxy [WebSocketHandler] []
             (onOpen [c]
@@ -29,9 +33,7 @@
               (println "closed" c)
               (swap! ws-connections disj c))
             (onMessage [c j] (println c j))))
-    (.add (StaticFileHandler. "."))
-    (.start))
-  (doto (WebServers/createWebServer 9001)
+    ;; Mount EventSourceHandler at /es
     (.add "/es"
           (proxy [EventSourceHandler] []
             (onOpen [c]
